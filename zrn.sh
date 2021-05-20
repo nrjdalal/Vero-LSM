0a98a6a2e64229074a7c090d91ab2ccadd32af9f
Neeraj Dalal <nd941z@gmail.com>
2021-05-21 00:23:26 +0530

    210520-185326

--- zrn.sh
+++ zrn.sh
@@ -7,19 +7,24 @@ version="0.0.6-rc"
 # ------------------------------ 00) functions ------------------------------
 
 rootCheck() {
+
   if [[ $(id -u) -ne 0 ]]; then
     echo
     echo "error: given command must be run using $(tput setaf 3)sudo$(tput sgr0)"
     echo
     exit 1
   fi
+
 }
 
 checkLatest() {
+
   latest_version="$(curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/zrn.sh | sed -n '3p' | sed 's/version="//' | sed 's/"//')"
+
 }
 
 addZrn() {
+
   checkLatest
   curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/zrn.sh >$ZRN
   chmod +x $ZRN
@@ -28,6 +33,7 @@ addZrn() {
   echo "success: $(tput setaf 2)added zrn $latest_version$(tput sgr0)"
   echo
   exit 0
+
 }
 
 # ------------------------------ 01) installation ------------------------------
@@ -59,8 +65,15 @@ case $1 in
 version)
 
   checkLatest
-  echo "Current version - $version"
-  echo "Latest version - $latest_version"
+
+  echo
+  if [[ "$version" == "$latest_version" ]]; then
+    echo "you're using the latest verion $version"
+  else
+    echo "newer version available $version -> $(tput setaf 2)$latest_version$(tput sgr0)"
+    echo "run $(tput setaf 3)sudo zrn update$(tput sgr0) to upgrade"
+  fi
+  echo
 
   ;;
 
@@ -76,6 +89,7 @@ update)
 # ------------------------------ SELF HELP ------------------------------
 
 *)
+
   echo
   echo "be careful what you wish for!"
   echo
