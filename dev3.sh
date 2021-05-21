7125649d5f679c318fd94424230f1e0ee2e3355b
Neeraj Dalal <nd941z@gmail.com>
2021-05-21 21:43:22 +0530

    210521-161322

--- dev3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source <(curl -s https://raw.githubusercontent.com/nrjdalal/zrn/master/utils.sh)
-
-echo "${Magenta}hello world!${Off}"
---
+++ dev4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mktemp utils.sh
+curl -s https://raw.githubusercontent.com/nrjdalal/zrn/master/utils.sh >/tmp/utils.sh
+
+cat /tmp/utils.sh
+
+# echo "${Magenta}hello world!${Off}"
