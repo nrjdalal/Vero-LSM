e0f1a8028b3f9adc5d5480607c4098bafaf1d284
Neeraj Dalal <nd941z@gmail.com>
2021-05-21 21:44:27 +0530

    210521-161427

--- dev4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mktemp utils.sh
-curl -s https://raw.githubusercontent.com/nrjdalal/zrn/master/utils.sh >/tmp/utils.sh
-
-cat /tmp/utils.sh
-
-# echo "${Magenta}hello world!${Off}"
---
+++ dev5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/utils.sh >/tmp/utils.sh
+
+cat /tmp/utils.sh
+
+# echo "${Magenta}hello world!${Off}"
