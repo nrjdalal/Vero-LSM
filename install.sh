3768bfd1cda19ce0bfcecec4249d57a9a87e8670
Neeraj Dalal <nd941z@gmail.com>
2021-05-22 01:36:39 +0530

    Fri May 21 20:06:39 UTC 2021

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+set -u
 
 abort() {
   printf "%s\n" "$@"
   exit 1
 }
 
-OS="$(uname)"
-if [[ "$OS" == "Linux" ]]; then
-  HOMEBREW_ON_LINUX=1
-elif [[ "$OS" != "Darwin" ]]; then
-  abort "Homebrew is only supported on macOS and Linux."
+if [ -z "${BASH_VERSION:-}" ]; then
+  abort "Bash is required to interpret this script."
+else
+  abort "You no how to bash, nice!"
 fi
+
+# # getting utilities
+# file=$(mktemp)
+# cat utils.sh >$file
+# # curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/install.sh >$file
+# source $file
+# rm $file
+
+# OS="$(uname)"
+# if [[ "$OS" == "Linux" ]]; then
+#   HOMEBREW_ON_LINUX=1
+# elif [[ "$OS" != "Darwin" ]]; then
+#   abort "Homebrew is only supported on macOS and Linux."
+# fi
