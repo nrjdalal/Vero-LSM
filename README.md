9b8fd974de7bbf70a90225d2e07725037e732da0
Neeraj Dalal <nd941z@gmail.com>
2021-05-22 18:49:11 +0530

    210522-131911

---
+++ README.md
@@ -0,0 +1,7 @@
+# zrn installer
+
+## Install zrn (on macOS or Linux)
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/install.sh"
+```
--- install.sh
+++ install.sh
@@ -5,29 +5,25 @@ curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/utils.sh >$file
 source $file
 rm $file
 
-# if [[ -z "${BASH_VERSION}" || -z "${ZSH_VERSION}" ]]; then
-# else
-#   Abort "Bash/Zsh is required to interpret this script."
-# fi
-
-# OS="$(uname)"
-# if [[ "$OS" == "Linux" ]]; then
-#   LOCATION=/usr/bin
-# elif [[ "$OS" == "Darwin" ]]; then
-#   LOCATION=/usr/local/bin
-# else
-#   Abort "zrn is only supported on macOS and Linux."
-# fi
+if [[ -z "${BASH_VERSION}" ]]; then
+  FAILURE "Bash is required to interpret this script."
+fi
 
 OS="$(uname)"
 case $OS in
 Linux)
+  echo
   success "Linux detected. Installing zrn."
   LOCATION=/usr/bin
   ;;
 Darwin)
+  echo
   success "macOS detected. Installing zrn."
   LOCATION=/usr/local/bin
   ;;
 *) FAILURE "zrn is only supported on macOS and Linux." ;;
 esac
+
+curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/bin/zrn.sh >$LOCATION
+chmod +x $LOCATION
+SUCCESS "zrn is installed."
