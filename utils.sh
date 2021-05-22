032f4094ccb63287a1892b6460ed61562eca08d7
Neeraj Dalal <nd941z@gmail.com>
2021-05-22 21:46:50 +0530

    210522-161650

--- utils.sh
+++ utils.sh
@@ -26,7 +26,7 @@ success() {
 }
 
 SUCCESS() {
-  printf "\n${BOLD}${green} %s ${OFF}\n\n" "$@"
+  printf "\n${BOLD}${green}%s${OFF}\n\n" "$@"
   exit 0
 }
 
@@ -35,7 +35,7 @@ failure() {
 }
 
 FAILURE() {
-  printf "\n${BOLD}${red} %s ${OFF}\n\n" "$@"
+  printf "\n${BOLD}${red}%s${OFF}\n\n" "$@"
   exit 1
 }
 
