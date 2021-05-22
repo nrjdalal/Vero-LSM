c8345ae66c693cafe7dceadf6ef8721e724e8b9a
Neeraj Dalal <nd941z@gmail.com>
2021-05-22 20:35:37 +0530

    210522-150537

--- bin/zrn.sh
+++ bin/zrn.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "zrn is awesome! heavenly! godly!"
+OS="$(uname)"
+case $OS in
+Linux)
+  LIB=/usr/lib/zrn
+  ;;
+Darwin)
+  LIB=/usr/local/lib/zrn
+  ;;
+esac
+
+source $LIB/utils.sh
+
+echo "Be ${BOLD} BOLD${OFF}"
