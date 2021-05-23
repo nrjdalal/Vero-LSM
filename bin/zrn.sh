44a71de324574bd16f73ee1a268437ad7c6c887f
Neeraj Dalal <nd941z@gmail.com>
2021-05-23 17:38:12 +0530

    210523-120812

--- bin/zrn.sh
+++ bin/zrn.sh
@@ -14,4 +14,6 @@ source $LIB/utils.sh
 
 case $1 in
 adduser) src "https://raw.githubusercontent.com/nrjdalal/zrn/master/_linux/adduser.sh" ;;
+update) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/nrjdalal/zrn/master/install.sh)" ;;
+upkeep) src "https://raw.githubusercontent.com/nrjdalal/zrn/master/_linux/upkeep.sh" ;;
 esac
