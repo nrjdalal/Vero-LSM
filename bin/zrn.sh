701cb797fd21fd7c4e003a2ef6a3d82223e8bbdc
Neeraj Dalal <nd941z@gmail.com>
2021-05-24 12:17:19 +0530

    210524-064719

--- bin/zrn.sh
+++ bin/zrn.sh
@@ -12,11 +12,9 @@ esac
 
 source $LIB/utils.sh
 
-echo $#
-echo $1
-echo $2
+IP=("$@")
 
-case $1 in
+case ${IP[0]} in
 adduser) src "${LIB_URL}/adduser.sh" ;;
 install) src "${LIB_URL}/install.sh" ;;
 update) /bin/bash -c "$(curl -fsSL ${URL}/install.sh)" ;;
--- lib/install.sh
+++ lib/install.sh
@@ -1,9 +1,4 @@
-echo "Reaching the script"
-echo $#
-echo $1
-echo $2
-
-case $2 in
+case ${IP[1]} in
 brew | homebrew) src "${LIB_URL}/install/homebrew.sh" ;;
 nginx) src "${LIB_URL}/install/nginx.sh" ;;
 node | nodejs) src "${LIB_URL}/install/nodejs.sh" ;;
