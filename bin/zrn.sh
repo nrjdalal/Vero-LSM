9150c739336549f4d84779edf71a7a613c43f8d1
Neeraj Dalal <nd941z@gmail.com>
2021-05-24 20:43:24 +0530

    210524-151324

--- bin/zrn.sh
+++ bin/zrn.sh
@@ -19,4 +19,5 @@ adduser) src "${LIB_URL}/adduser.sh" ;;
 install | i) src "${LIB_URL}/install.sh" ;;
 update) /bin/bash -c "$(curl -fsSL ${URL}/install.sh)" ;;
 upkeep) src "${LIB_URL}/upkeep.sh" ;;
+-v | -version | --version) src "${LIB_URL}/version.sh" ;;
 esac
