92464eea84eded7e70612955998449dcad66386a
Neeraj Dalal <nd941z@gmail.com>
2021-05-26 20:51:36 +0530

    210526-152136

--- lib/nginx.sh
+++ lib/nginx.sh
@@ -1,3 +1,4 @@
 case ${X[1]} in
 proxy-80) src "${LIB_URL}/nginx/proxy-80.sh" ;;
+proxy-443) src "${LIB_URL}/nginx/proxy-443.sh" ;;
 esac
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210526-152012"
+Version="210526-152136"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
