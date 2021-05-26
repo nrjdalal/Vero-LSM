85b50da51f1cb6255ab85f138a1b57b8cb02f82f
Neeraj Dalal <nd941z@gmail.com>
2021-05-26 08:05:06 +0530

    210526-023506

--- lib/install/nginx.sh
+++ lib/install/nginx.sh
@@ -2,9 +2,7 @@ _LINUX
 _ONLYSUDO
 
 echo
-curl -fsSL https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyrings/nginx_signing.gpg <<AUTOIP
-y
-AUTOIP
+curl -fsSL https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyrings/nginx_signing.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nginx_signing.gpg] http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" >/etc/apt/sources.list.d/nginx.list
 apt update
 apt install nginx -y
