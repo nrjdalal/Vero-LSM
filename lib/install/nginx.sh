f9be73570d095cabc22ddfaeda7626b109c45122
Neeraj Dalal <nd941z@gmail.com>
2021-05-26 07:45:25 +0530

    210526-021525

--- lib/install/nginx.sh
+++ lib/install/nginx.sh
@@ -2,5 +2,9 @@ _LINUX
 _ONLYSUDO
 
 echo
+curl -fsSL https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyrings/nginx_signing.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nginx_signing.gpg] http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" >/etc/apt/sources.list.d/nginx.list
+apt update
 apt install nginx -y
+nginx
 echo
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210524-172452"
+Version="210526-021525"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
