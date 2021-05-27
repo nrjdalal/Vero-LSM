8136e81d4ce9e64d99845e0ed21fcc01bc610eee
Neeraj Dalal <nd941z@gmail.com>
2021-05-27 08:52:59 +0530

    210527-032259

--- lib/nginx/proxy-443.sh
+++ lib/nginx/proxy-443.sh
@@ -3,7 +3,10 @@ _ONLYSUDO
 
 echo
 
+echo
 read -p "Enter domain name ~ " DOMAIN
+echo
+echo "Ports in use: "${magenta}$(grep -Eho "127.0.0.1:.{4}" /etc/nginx/sites-available/*.conf | awk '{print substr($0,length($0)-3)}')${OFF}
 read -p "Enter proxy :port ~ " PORT
 
 cat >/etc/nginx/sites-available/$DOMAIN.80.conf <<CONF
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210527-032126"
+Version="210527-032259"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
