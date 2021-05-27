e93039ca8e36b4c5414cdce2fd2b78af0414fbdd
Neeraj Dalal <nd941z@gmail.com>
2021-05-27 06:13:22 +0530

    210527-004322

---
+++ lib/nginx/root-80.sh
@@ -0,0 +1,26 @@
+_LINUX
+_ONLYSUDO
+
+echo
+
+read -p "Enter domain name ~ " DOMAIN
+read -p "Enter proxy :port ~ " PORT
+
+cat >/etc/nginx/sites-available/$DOMAIN.80.conf <<CONF
+server {
+  server_name $DOMAIN www.$DOMAIN;
+
+  listen 80;
+  listen [::]:80;
+
+  root /var/www/$DOMAIN
+}
+CONF
+
+ln -s /etc/nginx/sites-available/$DOMAIN.80.conf /etc/nginx/sites-enabled/$DOMAIN.80.conf 2>/dev/null
+
+nginx -s reload
+
+echo "Config created. Verify by ~ $(tput setaf 3)cat /etc/nginx/sites-enabled/$DOMAIN.80.conf$(tput sgr0)"
+
+echo
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210527-002514"
+Version="210527-004322"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
