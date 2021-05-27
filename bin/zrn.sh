47d52251f0bea8ce738a3397778583270c180a5b
Neeraj Dalal <nd941z@gmail.com>
2021-05-27 05:55:14 +0530

    210527-002514

--- bin/zrn.sh
+++ bin/zrn.sh
@@ -18,8 +18,8 @@ case ${X[0]} in
 adduser) src "${LIB_URL}/adduser.sh" ;;
 certbot) src "${LIB_URL}/certbot.sh" ;;
 install | i) src "${LIB_URL}/install.sh" ;;
+maintain) src "${LIB_URL}/maintain.sh" ;;
 nginx) src "${LIB_URL}/nginx.sh" ;;
 update) /bin/bash -c "$(curl -fsSL ${URL}/install.sh)" ;;
-upkeep) src "${LIB_URL}/upkeep.sh" ;;
 -v | -version | --version) src "${LIB_URL}/version.sh" ;;
 esac
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210527-001617"
+Version="210527-002514"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
