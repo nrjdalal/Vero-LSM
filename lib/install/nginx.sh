7792106ae70c9d746c48cb468e5ac50dc43c1c26
Neeraj Dalal <nd941z@gmail.com>
2021-05-27 05:46:17 +0530

    210527-001617

--- lib/install/nginx.sh
+++ lib/install/nginx.sh
@@ -2,10 +2,18 @@ _LINUX
 _ONLYSUDO
 
 echo
+
 curl -fsSL https://nginx.org/keys/nginx_signing.key | gpg --dearmor -o /usr/share/keyrings/nginx_signing.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nginx_signing.gpg] http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" >/etc/apt/sources.list.d/nginx.list
 apt update
 apt install nginx -y
-systemctl enable --now nginx
+
 mkdir -p /etc/nginx/{sites-available,sites-enabled}
+if ! grep -q "include /etc/nginx/sites-enabled/\*;" /etc/nginx/nginx.conf; then
+  sed -i 's#    include /etc/nginx/conf.d/\*.conf;#    include /etc/nginx/conf.d/\*.conf;\n    include /etc/nginx/sites-enabled/\*;#' /etc/nginx/nginx.conf
+fi
+
+systemctl enable --now nginx
+nginx -t && nginx -s reload
+
 echo
--- utils/utils.sh
+++ utils/utils.sh
@@ -1,4 +1,4 @@
-Version="210526-184343"
+Version="210527-001617"
 
 URL="https://raw.githubusercontent.com/nrjdalal/zrn/master"
 BIN_URL="${URL}/bin"
